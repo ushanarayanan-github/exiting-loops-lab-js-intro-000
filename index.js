d740b7472b563ad418da507bb456fbc87eac38c1
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-20 04:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function breakOut(array, changeValue, stopValue) {
 for (let i=0;i<array.length;i++) {
- if (array[i] != stopValue) {
+ if (array[i] !== stopValue) {
    array[i] = changeValue
  }
  else {
    break;
  }
 }
-
+return array
 }
