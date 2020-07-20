5d094cd00dd6af12e9c3ac6d4926f3664c3de412
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-20 05:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ for (let i=0;i<array.length;i++) {
 }
 return array
 }
+
+function keepGoing(array, changeValue, stopValue) {
+for (let i=0;i<array.length;i++) {
+ if (array[i] !== stopValue) {
+   continue;
+ }
+ array[i] = changeValue
+}
+return array
+}
