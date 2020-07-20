8b81cd3733aee6c46cbd61c252eece5925b77ffb
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-20 05:22:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ return array
 }
 
 function findBy(array, findFn) {
-
-
+  for (let i=0;i<array.length;i++) {
+   if (array[i] === findFn()) {
+     return array[i]   
+   }
+  }
+  return null
 }
