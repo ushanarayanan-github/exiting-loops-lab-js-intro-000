3f414b89edac77caf71c5b86477fa6b3c4e6093f
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-20 05:15:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ return array
 
 function keepGoing(array, changeValue, skipValue) {
 for (let i=0;i<array.length;i++) {
- if (array[i] !== skipValue) {
+ if (array[i] != skipValue) {
    continue;
  }
  array[i] = changeValue
